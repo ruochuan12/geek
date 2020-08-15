80c832074867ea6a4d082742338ed332c1ab7a2e
lxchuan12 <lxchuan12@163.com>
2020-08-15 23:09:11 +0800

    feat: init react project

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+fetch('https://time.geekbang.org/serv/v1/column/details', {
+  method: 'POST',
+})
+.then(res => res.json())
+.then(res => {
+  console.log('fetch', res);
+});
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
